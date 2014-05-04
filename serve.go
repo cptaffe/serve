d96189af27fca40281b376c16469c80510c301e7
Connor Taffe <cpaynetaffe@gmail.com>
2014-05-04 18:21:48 -0500

    Fixed arg parsing

--- serve.go
+++ serve.go
@@ -1,71 +1,68 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"errors"
 )
 
 const (
 	// colors (linux)
-	BLUE   = "\033[36m"
-	RED    = "\033[31m"
-	ENDC   = "\033[0m"
-	BEGIN  = BLUE + "#" + ENDC
-	ERROR  = "("+RED+"!"+ENDC+")"
+	BLUE  = "\033[36m"
+	RED   = "\033[31m"
+	ENDC  = "\033[0m"
+	BEGIN = BLUE + "#" + ENDC
+	ERROR = "(" + RED + "!" + ENDC + ")"
 )
 
-type Server struct{
-	Dir string
+type Server struct {
+	Dir  string
 	Port string
-	Sub string
+	Sub  string
 }
 
-func ParseArgs() (*Server, error){
+func ParseArgs() (*Server, error) {
 	s := new(Server)
-	if len(os.Args) < 2 {
-		return s, errors.New("not enough args")
-	} else {
-		for i := 1; i < len(os.Args); i++ {
-			if os.Args[i] == "-p" {
-				s.Port = os.Args[i++]
-			}
-			else if os.Args[i] == "-d" {
-				s.Dir = os.Args[i++]
-			}
-			else if os.Args[i] == "-s" {
-				s.Sub = os.Args[i++]
-			}
-			else if os.Args[i] == "-h" || os.Args[i] == "help" {
-				fmt.Printf("serve [ -p port | -d dir ]")
-			}
-			} else {
-				s.Port = os.Args[i]
-			}
+	for i := 1; i < len(os.Args); i++ {
+		if os.Args[i] == "-p" {
+			i++
+			s.Port = os.Args[i]
+		} else if os.Args[i] == "-d" {
+			i++
+			s.Dir = os.Args[i]
+		} else if os.Args[i] == "-s" {
+			i++
+			s.Sub = os.Args[i]
+		} else if os.Args[i] == "-h" || os.Args[i] == "help" {
+			fmt.Printf("serve [ -p port | -d dir | -s sub ]\n")
+			return s, errors.New("requested help")
+		} else {
+			s.Port = os.Args[i]
 		}
-		if s.Port == "" {
-			s.Port = "4000"
-		}
-		if s.Dir == "" {
-			s.Dir = "."
-		}
-		if s.Sub == "" {
-			s.Sub = "/"
-		}
-		return s, nil
 	}
+	if s.Port == "" {
+		s.Port = "4000"
+	}
+	if s.Dir == "" {
+		s.Dir = "."
+	}
+	if s.Sub == "" {
+		s.Sub = "/"
+	}
+	return s, nil
 }
 
 func main() {
 	s, err := ParseArgs()
 	if err != nil {
-		fmt.Printf(BEGIN+" %s "+ERROR, err)
+		fmt.Printf(BEGIN+" %s "+ERROR+"\n", err)
+		return
 	}
 
 	http.Handle(s.Sub, http.StripPrefix(s.Sub, http.FileServer(http.Dir(s.Dir))))
 	// Serves at port
 	fmt.Printf("\033[36m#\033[0m Serving at :%s\n", s.Port)
-	log.Fatal(http.ListenAndServe(":"+s.Port), nil))
+	log.Fatal(http.ListenAndServe(":"+s.Port, nil))
 }
