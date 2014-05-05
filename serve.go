5ec6c4a335ef2ac082b6786e7a0cf772ca99ea7c
Connor Taffe <cpaynetaffe@gmail.com>
2014-05-05 09:33:57 -0500

    Flag pkg usage

--- serve.go
+++ serve.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 const (
@@ -24,33 +23,11 @@ type Server struct {
 }
 
 func ParseArgs() (*Server, error) {
-	s := new(Server)
-	for i := 1; i < len(os.Args); i++ {
-		if os.Args[i] == "-p" {
-			i++
-			s.Port = os.Args[i]
-		} else if os.Args[i] == "-d" {
-			i++
-			s.Dir = os.Args[i]
-		} else if os.Args[i] == "-s" {
-			i++
-			s.Sub = os.Args[i]
-		} else if os.Args[i] == "-h" || os.Args[i] == "help" {
-			fmt.Printf("serve [ -p port | -d dir | -s sub ]\n")
-			return s, errors.New("requested help")
-		} else {
-			s.Port = os.Args[i]
-		}
-	}
-	if s.Port == "" {
-		s.Port = "4000"
-	}
-	if s.Dir == "" {
-		s.Dir = "."
-	}
-	if s.Sub == "" {
-		s.Sub = "/"
-	}
+	dir := flag.String("d", ".", "directory")
+	port := flag.String("p", "4000", "port")
+	sub := flag.String("s", "/", "server prefix")
+	flag.Parse()
+	s := &Server{Dir: *dir, Port: *port, Sub: *sub}
 	return s, nil
 }
 
